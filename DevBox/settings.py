eec21a25e54f9fc40cf27126e7ac371bd15c383d
jobm <jobmwaniki18@gmail.com>
2015-12-13 19:03:29 +0300

    modified settings.py

--- DevBox/settings.py
+++ DevBox/settings.py
@@ -13,12 +13,15 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 import dj_database_url
-
+DATABASES = {}
 DATABASES['default'] =  dj_database_url.config()
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+# Allow all host headers
 ALLOWED_HOSTS = ['*']
+
 import os
 
 from shared.config import *
