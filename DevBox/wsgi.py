174eff2723a7b1cab797c8fd7fe32a2312f35c5a
jobm <jobmwaniki18@gmail.com>
2015-12-13 20:35:33 +0300

    altered wsgi.py

--- DevBox/wsgi.py
+++ DevBox/wsgi.py
@@ -15,4 +15,4 @@ from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DevBox.settings")
 
-application = get_wsgi_application(get_wsgi_application())
+application = Cling(get_wsgi_application())
