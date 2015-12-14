097873e7d5796bc7c117cb34be43637c1aacead3
jobm <jobmwaniki18@gmail.com>
2015-12-14 16:52:42 +0300

    problems deploying

--- DevBox/wsgi.py
+++ DevBox/wsgi.py
@@ -8,12 +8,12 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DevBox.settings")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DevBox.settings")
 
 application = Cling(get_wsgi_application())
 application = DjangoWhiteNoise(application)
