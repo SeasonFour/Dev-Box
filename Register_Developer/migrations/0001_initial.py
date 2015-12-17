2a00756e91e3f6b9929119ee408125419c42572a
jobm <jobmwaniki18@gmail.com>
2015-12-17 20:32:15 +0300

    reset the databse

--- Register_Developer/migrations/0001_initial.py
+++ Register_Developer/migrations/0001_initial.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Developer',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
                 ('user_name', models.CharField(blank=True, max_length=50)),
                 ('first_name', models.CharField(max_length=50)),
                 ('last_name', models.CharField(max_length=50)),
@@ -22,11 +22,11 @@ class Migration(migrations.Migration):
                 ('date_created', models.DateTimeField(auto_now=True)),
                 ('date_updated', models.DateTimeField(auto_now=True)),
                 ('bio', models.TextField(verbose_name='About You', max_length=500)),
-                ('profile_picture', models.ImageField(upload_to='', blank=True)),
+                ('profile_picture', models.ImageField(blank=True, upload_to='')),
                 ('website_url', models.URLField(null=True)),
-                ('years_exp', models.IntegerField(default=1, verbose_name='Years of experience', validators=[django.core.validators.MinValueValidator(0), django.core.validators.MaxValueValidator(50)], blank=True)),
-                ('software_title', models.CharField(default='Back-End', blank=True, max_length=50)),
-                ('languages', models.CharField(verbose_name='Programming Languages', blank=True, max_length=1000)),
+                ('years_exp', models.IntegerField(blank=True, default=1, verbose_name='Years of experience', validators=[django.core.validators.MinValueValidator(0), django.core.validators.MaxValueValidator(50)])),
+                ('software_title', models.CharField(blank=True, default='Back-End', max_length=50)),
+                ('languages', models.CharField(blank=True, verbose_name='Programming Languages', max_length=1000)),
                 ('is_developer', models.BooleanField(default=False)),
             ],
             options={
@@ -36,9 +36,9 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Portfolio',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
                 ('title', models.CharField(max_length=50)),
-                ('image', models.ImageField(upload_to='', blank=True)),
+                ('image', models.ImageField(blank=True, upload_to='portfolios')),
                 ('description', models.TextField()),
                 ('github_link', models.URLField()),
                 ('date_created', models.DateTimeField(auto_now=True)),
